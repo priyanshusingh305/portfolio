7a93cfb1c90493a58405952277988ff715747bf0
priyanshusingh305 <singhpriyanshu305@gmail.com>
2021-09-06 01:29:10 +0530

    some changes

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+   <div>
+    <div className="container">
+      priyanshu singh portfolio coming soon...
+    </div>
+    </div>
+  );
+}
+
+export default App;
